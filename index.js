fbe3000278be9fd01685f812dbd3b9b6a233fa1f
pantho0 <panthomashreky6@gmail.com>
2023-11-17 18:37:22 +0600

    user role changing api end point created

--- index.js
+++ index.js
@@ -49,6 +49,18 @@ async function run() {
       res.send(result)
     });
 
+    app.patch("/user/admin/:id", async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set :{
+          role : "admin"
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
     app.delete("/users/:id", async(req,res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
